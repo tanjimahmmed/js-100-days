1de93cc44fa0b4c714a28ac2a7a9e41bbaa88cf3
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2021-01-03 11:32:40 +0600

    Counter app

---
+++ counter-app/app.js
@@ -0,0 +1,29 @@
+// ---set initial count
+let count = 0;
+
+// ---select value and buttons
+const value = document.querySelector('#value');
+const btnS = document.querySelectorAll('.btn');
+
+btnS.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }else if(styles.contains('increase')){
+            count++;
+        }else {
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = 'green'
+        }
+        if(count < 0){
+            value.style.color = 'red';
+        }
+        if(count === 0){
+            value.style.color = '#222';
+        }
+        value.textContent = count;
+    });
+});
